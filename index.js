06acc35a73930938c8985064a7c57b951b1a6f87
Alex De la Cruz <alexxruff@yahoo.com>
2018-03-25 23:55:21 -0600

    Se agregó métodos Delete y Put(update)

--- index.js
+++ index.js
@@ -49,9 +49,30 @@ app.post('/api/product', (req, res) => {
 	})
 })
 
-app.put('/api/product/:productId', (req, res) => {})
+app.put('/api/product/:productId', (req, res) => {
+	let productId = req.params.productId
+	let update = req.body
 
-app.delete('api/product/:productId', (req, res) => {})
+	Product.findByIdAndUpdate(productId, update, (err, productUpdated) => {
+		if (err) res.status(500).send({ message: `Error al actualizar el producto: ${err}` })
+
+		res.status(200).send({ product: productUpdated })
+	})
+})
+
+app.delete('/api/product/:productId', (req, res) => {
+	let productId = req.params.productId
+
+	Product.findById(productId, (err, product) => {
+		if (err) res.status(500).send({ message: `Error al eliminar el producto: ${err}` })
+
+		product.remove(err => {
+			if (err) res.status(500).send({ message: `Error al eliminar el producto1: ${err}` })
+
+			res.status(200).send({ message: 'El producto ha sido eliminado' })
+		})
+	})
+})
 
 // Conexión a mongo con mongoose
 mongoose.connect('mongodb://localhost:27017/shop', (err, res) => {
