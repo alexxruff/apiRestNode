f8165056f9a73978d4c3b0aaa427c9ad60020e2b
Alex De la Cruz <alexxruff@yahoo.com>
2018-03-25 19:06:24 -0600

    Se agrego Mongoose al proyecto para conectar la api a mongodb

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 const app = express()
 const port = process.env.PORT || 3001
@@ -24,6 +25,13 @@ app.put('/api/product/:productId', (req, res) => {})
 
 app.delete('api/product/:productId', (req, res) => {})
 
-app.listen(port, () => {
-	console.log(`API REST Corriendo en http: //localhost: ${port}`)
+mongoose.connect('mongodb://localhost:27017/shop', (err, res) => {
+	if (err) {
+		return console.log(`Error al conectar a la base de datos: ${err}`)
+	}
+	console.log('Conexión a la base de datos exitosa... ')
+
+	app.listen(port, () => {
+		console.log(`API REST Corriendo en http: //localhost: ${port}`)
+	})
 })
