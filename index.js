f3ec7ac88ce9e148fd86217388b3946ef2fb55b6
Alex De la Cruz <alexxruff@yahoo.com>
2018-03-25 18:33:21 -0600

    Se agregaron algunos métodos

--- index.js
+++ index.js
@@ -9,9 +9,20 @@ const port = process.env.PORT || 3001
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-app.get('/hola/:name', (req, res) => {
-	res.send({ menssage: `Hola ${req.params.name}` })
+app.get('/api/product', (req, res) => {
+	res.send(200, { products: [] })
 })
+
+app.get('/api/product/:preductId', (req, res) => {})
+
+app.post('/api/product', (req, res) => {
+	console.log(req.body)
+	res.status(200).send({ message: 'El producto se ha recibido' })
+})
+
+app.put('/api/product/:productId', (req, res) => {})
+
+app.delete('api/product/:productId', (req, res) => {})
 
 app.listen(port, () => {
 	console.log(`API REST Corriendo en http: //localhost: ${port}`)
