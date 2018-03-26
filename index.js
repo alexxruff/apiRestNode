ef13723703afbb136ba6ccdce01310d13970ed61
Alex De la Cruz <alexxruff@yahoo.com>
2018-03-25 19:35:44 -0600

    Estamos guardando en mongo mediante metodo POST

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 
+const Product = require('./models/product')
+
 const app = express()
 const port = process.env.PORT || 3001
 
@@ -17,14 +19,28 @@ app.get('/api/product', (req, res) => {
 app.get('/api/product/:preductId', (req, res) => {})
 
 app.post('/api/product', (req, res) => {
+	console.log('POST /api/product')
 	console.log(req.body)
-	res.status(200).send({ message: 'El producto se ha recibido' })
+
+	let product = new Product()
+	product.name = req.body.name
+	product.picture = req.body.picture
+	product.price = req.body.price
+	product.category = req.body.category
+	product.description = req.body.description
+
+	product.save((err, productStored) => {
+		if (err) res.status(500).send({ message: `Error al guardar en la base de datos: ${err}` })
+
+		res.status(200).send({ product: productStored })
+	})
 })
 
 app.put('/api/product/:productId', (req, res) => {})
 
 app.delete('api/product/:productId', (req, res) => {})
 
+// Conexión a mongo con mongoose
 mongoose.connect('mongodb://localhost:27017/shop', (err, res) => {
 	if (err) {
 		return console.log(`Error al conectar a la base de datos: ${err}`)
