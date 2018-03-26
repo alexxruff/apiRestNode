6f6b5893a635110a49263828c01c02c47a016407
Alex De la Cruz <alexxruff@yahoo.com>
2018-03-25 18:00:17 -0600

    Se agregó express y se ejecuta el servidor con nodemon

---
+++ index.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+const port = process.env.PORT || 3001
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.get('/hola/:name', (req, res) => {
+	res.send({ menssage: `Hola ${req.params.name}` })
+})
+
+app.listen(port, () => {
+	console.log(`API REST Corriendo en http: //localhost: ${port}`)
+})
